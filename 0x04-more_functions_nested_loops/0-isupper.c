794a164cd9f2aaab5aa240cd9fbd73b95b11c8f7
Soukaina110 <soukaina.elmoquialle@gmail.com>
2023-06-23 00:56:19 +0100

    projet

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *  * _isupper - check if c is upper
- *   *
- *    * @c: input
- *     *
- *      * Return: 1 if its upper and 0 if not
- *      */
+ * _isupper - check if c is upper
+ *
+ * @c: input
+ *
+ * Return: 1 if its upper and 0 if not
+*/
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
