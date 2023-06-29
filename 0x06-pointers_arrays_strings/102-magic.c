0271b8b75c2b9dd2803710ca49e27e87c107adb8
soukaina <myemail@gmail.com>
2023-06-29 09:02:29 -0700

    projet

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+ 	 int n;
+ 	 int a[5];
+ 	 int *p;
  
-  a[2] = 1024;
-  p = &n;
+ 	 a[2] = 1024;
+ 	 p = &n;
   /*
    * write your line of code here...
    * Remember:
