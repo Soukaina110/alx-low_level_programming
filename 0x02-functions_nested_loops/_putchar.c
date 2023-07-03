f97674675a6c7252c72427bbe7974101b99ce638
soukaina <myemail@gmail.com>
2023-07-03 09:06:07 -0700

    projet

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putchar - writes thecharacter c to stdout 
- * @c: the character to priny
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -11,3 +11,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
-int putchar(char);
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,7 +11,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int,int);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
 #endif
+
