219096bf92162dec180869ddd3c1b27d684e014d
soukaina <myemail@gmail.com>
2023-07-05 10:34:52 -0700

    projet

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -12,13 +13,15 @@ int print_sign(int n)
         {
                 _putchar('+');
                 return (1);
-        } else if (n == 0)
+        }
+        else if (n == 0)
         {
-                _putchar(48);
+                _putchar('0');
                 return (0);
-        } else if (n < 0)
+        }
+        else if (n < 0)
         {
                 _putchar('-');
         }
-                return (-1);
+        return (-1);
 }
